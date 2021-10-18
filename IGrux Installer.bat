30d15ea613a36b507840057091bc36ab745c46d6
MALYMATI2007 <75562552+MALYMATI2007@users.noreply.github.com>
2021-10-18 22:13:46 +0200

    Update IGrux Installer.bat

--- IGrux Installer.bat
+++ IGrux Installer.bat
@@ -1,5 +1,6 @@
 @echo off
 git clone https://github.com/MALYMATI2007/IGrux-git_install.git
-pip install colorama
-pip install audioplayer
+python -m pip install colorama
+python -m pip install audioplayer
+python -m pip install pypresence
 pause
